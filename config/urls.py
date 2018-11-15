3932f9a60d6627f2f5cca65b7abc9daaf1cfca2f
Tom Mortimer-Jones <tom@morty.co.uk>
2018-11-15 10:02:27 +0000

    [EOT-49] Update Django admin heading

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = 'Expressways Administration'
+admin.site.site_title = 'Expressways Administration'
+
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
