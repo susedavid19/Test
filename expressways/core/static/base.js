65db7a6f85129cb9e6601c4e69ecb2f4c7233b8e
Tom Mortimer-Jones <tom@morty.co.uk>
2018-12-14 11:20:42 +0000

    Don't allow the spinner to be dismissed

--- expressways/core/static/base.js
+++ expressways/core/static/base.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, {});
+    var instances = M.Modal.init(elems, {'dismissible': false});
 });
 
 
