3f2a4d92bf5a59e91c3153bda64ac2606845b406
Tom Mortimer-Jones <tom@morty.co.uk>
2018-12-19 09:22:00 +0000

    Switch from RabbitMQ to Redis for the Queue
    Because RabbitMQ doesn't permanently store results it was difficult to see when
    a task had failed because the result was being consumed by flower and wasn't
    available to the application.

--- expressways/calculation/celery.py
+++ expressways/calculation/celery.py
@@ -1,11 +1,3 @@
 from celery import Celery
 
-app = Celery('tasks', backend='rpc://', broker='pyamqp://guest@queue//')
-app.conf.broker_transport_options = {
-    'max_retries': 4,
-    'interval_start': 0,
-    'interval_step': 0.5,
-    'interval_max': 3,
-}
-# https://github.com/celery/celery/issues/4895
-app.conf.broker_heartbeat = 0
+app = Celery('tasks', backend='redis://queue:6379/0', broker='redis://queue:6379/0')
