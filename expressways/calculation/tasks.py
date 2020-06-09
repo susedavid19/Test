7df00afcedd2c238ddb711f30891f48379e6d2eb
Ashraf Anwar <ashraf.anwar-apandi@wsp.com>
2020-06-09 17:52:02 +0100

    Add incident cleared calc; show on result page

--- expressways/calculation/tasks.py
+++ expressways/calculation/tasks.py
@@ -17,6 +17,7 @@ def calculate(self, config_ids, items, component_ids= None):
     df = pd.DataFrame()
     header = load_header_data(r'expressways/calculation/models', 'csv')
     freqs_list = []
+    less_an_hour_list = []
     if component_ids:
         freq_change = []
         dur_change = []
@@ -28,12 +29,17 @@ def calculate(self, config_ids, items, component_ids= None):
             dur_change.append(item['duration_change'])
             freq_change.append(item['frequency_change'])
 
+            if item['duration'] <= 60 and item['incidents_cleared']:
+                less_an_hour_list.append(item)
+
         freqs_list = frequency_change(freqs_list, freq_change)
         freqs_list = duration_bin(freqs_list, dur_change, durations)
     else:
         for item in items:
             freqs_list.append(item['frequency'])
 
+            if item['duration'] <= 60 and item['incidents_cleared']:
+                less_an_hour_list.append(item)
 
     freqs_list = norm_freqs(freqs_list)
     for i, item in enumerate(items):
@@ -52,6 +58,7 @@ def calculate(self, config_ids, items, component_ids= None):
                 str(item['flow']), freqs_list[i]
             )
 
+    objective_incident = incidents_cleared(less_an_hour_list, items)
     objective_pti = pti(df)
     objective_journey = acceptable_journeys(df)
     objective_speed = average_speed(df)
@@ -62,6 +69,7 @@ def calculate(self, config_ids, items, component_ids= None):
     if component_ids:
         result.component_ids = component_ids
     result.freq_list = freqs_list
+    result.objective_incident = objective_incident
     result.objective_pti = objective_pti
     result.objective_journey = objective_journey
     result.objective_speed = objective_speed
