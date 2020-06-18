edc7441b84ad8a661bff2203e8df542a0af40012
Ashraf Anwar <ashraf.anwar-apandi@wsp.com>
2020-06-18 09:06:59 +0100

    Add basic test

--- expressways/core/tests/test_proforma_loader.py
+++ expressways/core/tests/test_proforma_loader.py
@@ -2,8 +2,12 @@ from django.core.management import call_command
 from django.test import TestCase
 
 class TestProformaLoader(TestCase):
+    def setUp(self):
+        self.test_file = '/data/expressways/core/tests/data/Test_Proforma.xlsx'
+        self.test_sheet = 'Test Sheet'
+
     def test_loading_good_case(self):
-        call_command('load_oms_data', args=[], kwargs={})
+        call_command('load_oms_data', file=self.test_file, sheet=self.test_sheet)
         pass
 
     def test_loading_invalid_file(self):
