ebde71b370474df701a669ed0f298f592236c750
Ashraf Anwar <ashraf.anwar-apandi@wsp.com>
2020-06-19 10:12:32 +0000

    Merged in feature/EOT-128-references-field (pull request #40)
    Add references field and text updates
    
    * Test non-uid0
    
    * Chown static
    
    * Chown static
    
    * Added basic wrapper for project initialisation
    
    * Added environment variable to initContainer
    
    * Changed order of workDir
    
    * Adding sequence image
    
    * Revert the image
    
    * Run as wspreadonly user
    
    * Temporary work-around gunicorn
    
    * Separate the entrypoint for rancher
    
    * Made entrypoint executable
    
    * Upgrade django to 2.2.13; fix issue when running all service locally
    
    * Update pytest and failing tests
    
    * Add updated model files
    
    * Update calculation to replace dummy result values
    
    * Fixed deployment yml
    
    * Add log to troubleshoot
    
    * Add debug log
    
    * Add debug log
    
    * Add debug log
    
    * Add debug log
    
    * Add debug log
    
    * Add debug log
    
    * Update objectives with proper identifier
    
    * Update tests with new objective fields
    
    * Remove debug prints
    
    * Remove debug logging
    
    * Remove debug log
    
    * Remove debug log
    
    * Split test for baseline calc; add function desc
    
    * Add incident cleared in configuration
    
    * Add incident cleared calc; show on result page
    
    * Update existing tests
    
    * Update based on PR feedback
    
    * Update based on PR feedback
    
    * Add references field and enable in admin panel
    
    * Update frequency help text and change titles
    
    * Fix incidents_cleared field duplicates
    
    Approved-by: Kevin Cornwell <kevin.cornwell@wsp.com>

--- expressways/core/models.py
+++ expressways/core/models.py
@@ -114,11 +114,15 @@ class OccurrenceConfiguration(models.Model):
         default=70,
     )
     frequency = models.PositiveIntegerField(
-        help_text=_('Define the frequency of the occurrence (per mile per year)​')
+        help_text=_('Define the frequency of the occurrence (average per mile per year)​')
     )
     incidents_cleared = models.BooleanField(
         help_text=_('If ticked, incidents occurring as a result of this Operational Occurrence will be included in the ‘Incidents Cleared’ calculation (as defined by the "IncidentClearance" field code in ControlWorks data)'),
         default=False
+    )
+    references = models.TextField(
+        help_text=_('Provide references to data sources or a detailed explanation of any assumptions used e.g. calculations to determine frequencies or durations.'),
+        default=''
     )
     effect = models.ManyToManyField(
         DesignComponent,
@@ -146,11 +150,13 @@ class EffectIntervention(models.Model):
     )
     frequency_change = models.IntegerField(
         help_text=_(f'% change in frequency'),
-        validators=[MinValueValidator(-100)]
+        validators=[MinValueValidator(-100)],
+        verbose_name='Frequency change (%)',
     )
     duration_change = models.IntegerField(
         help_text=_(f'% change in duration'),
-        validators=[MinValueValidator(-100)]
+        validators=[MinValueValidator(-100)],
+        verbose_name='Duration change (%)',
     )
     justification = models.TextField(
         help_text=_('Provide a detailed explanation and references for the sources of the impact sources. Must be based on real data, published research or an assumption agreed with relevant subject matter experts')
